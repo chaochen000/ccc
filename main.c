d2126ddc98a6c7c8110e5a8fea9361286513ebde
chaochen000 <39374617+chaochen000@users.noreply.github.com>
2018-05-31 14:58:53 +0800

    指向非常數資料的常數指標

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #define SIZE 10
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void bubbleSort( int * array, unsigned int size);	//ptototype
+void bubbleSort( int *const array, unsigned int size);	//ptototype
 int main(void) 
 {
 	//initialize array a
